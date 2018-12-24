91c1c6255a3489e6ae70e28b8ea7097359fe624b
gespyrop <giwrgosspyropoulos@gmail.com>
2018-12-24 11:39:50 +0200

    Printing steps

--- exercise2.cpp
+++ exercise2.cpp
@@ -6,35 +6,60 @@
 
 using namespace std;
 
-string factor(){
+string factor(int tabs){
+	for(int i = 0; i < tabs; i++){
+		cout << "\t";
+	}
+
 	int r = rand() % 3;
 
 	switch(r){
 		case 0:
+			cout << "<factor> --> a" << endl;// << endl;
 			return "a";
 		case 1:
+			cout << "<factor> --> b" << endl;// << endl;
 			return "b";
 		default:
+			cout << "<factor> -->c" << endl;// << endl;
 			return "c";
 	}
 }
 
-string term(){
-	if(rand() % 2 == 0) return factor();
-	else return term() + "*" + factor();
+string term(int tabs){
+	for(int i = 0; i < tabs; i++){
+		cout << "\t";
+	}
+
+	if(rand() % 2 == 0){
+		cout << "<term> --> <factor>" << endl;
+		return factor(tabs + 1);
+	}
+	else{
+		cout << "<term> --> <term> * <factor>" << endl;
+		return term(tabs + 1) + " * " + factor(tabs + 1);
+	}
 }
 
-string expression(){
-	if(rand() % 2 == 0) return term();
-	else return expression() + "+" + term();
+string expression(int tabs){
+	for(int i = 0; i < tabs; i++){
+		cout << "\t";
+	}
+
+	if(rand() % 2 == 0){
+		cout << "<expression> --> <term>" << endl;
+		return term(tabs + 1);
+	}
+	else{
+		cout << "<expression> --> <expression> + <term>" << endl;
+		return expression(tabs + 1) + " + " + term(tabs + 1);
+	}
 }
 
 int main(){
 	srand(time(0));
 
-	cout << expression() << endl;
-
-	//TODO print steps
+	cout << endl << expression(0) << endl;
 
 	return 0;
 }
