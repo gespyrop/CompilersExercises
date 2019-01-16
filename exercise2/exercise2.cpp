68a638f4fd9d314e6aef567781879b1ba96ad4a1
gespyrop <giwrgosspyropoulos@gmail.com>
2019-01-16 17:35:10 +0200

    added comments in exercise2

--- exercise2/exercise2.cpp
+++ exercise2/exercise2.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+//Παράγοντας
 string factor(int tabs){
 	for(int i = 0; i < tabs; i++){
 		cout << "\t";
@@ -14,42 +15,51 @@ string factor(int tabs){
 	int r = rand() % 3;
 
 	switch(r){
+		//Ο παράγοντας γίνεται a
 		case 0:
 			cout << "<factor> --> a" << endl;
 			return "a";
+		//Ο παράγοντας γίνεται b
 		case 1:
 			cout << "<factor> --> b" << endl;
 			return "b";
+		//Ο παράγοντας γίνεται c
 		default:
 			cout << "<factor> -->c" << endl;
 			return "c";
 	}
 }
 
+//Όρος
 string term(int tabs){
 	for(int i = 0; i < tabs; i++){
 		cout << "\t";
 	}
 
+	//Ο όρος γίνεται παράγοντας
 	if(rand() % 2 == 0){
 		cout << "<term> --> <factor>" << endl;
 		return factor(tabs + 1);
 	}
+	//Ο όρος γίνεται όρος * παράγοντας
 	else{
 		cout << "<term> --> <term> * <factor>" << endl;
 		return term(tabs + 1) + " * " + factor(tabs + 1);
 	}
 }
 
+//Έκφραση
 string expression(int tabs){
 	for(int i = 0; i < tabs; i++){
 		cout << "\t";
 	}
 
+	//Η έκφραση γίνεται όρος
 	if(rand() % 2 == 0){
 		cout << "<expression> --> <term>" << endl;
 		return term(tabs + 1);
 	}
+	//Η έκφραση γίνεται έκφραση + όρος
 	else{
 		cout << "<expression> --> <expression> + <term>" << endl;
 		return expression(tabs + 1) + " + " + term(tabs + 1);
@@ -59,7 +69,7 @@ string expression(int tabs){
 int main(){
 	srand(time(0));
 
-	cout << endl << expression(0) << endl;
+	cout << endl << expression(0) << endl;	//Παραγωγή τυχαίας συμβολοσειράς απο την γραμματική
 
 	return 0;
 }
